1637a05c01c94a5dd4d718adc0f0525c8ecb81ea
vivian <1022620@hr.nl>
2025-09-18 13:54:28 +0200

    more motion

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {AnimatePresence, AnimateSharedLayout, motion} from "framer-motion";
+import React, {useEffect, useRef, useState} from "react";
+import {AnimatePresence, AnimateSharedLayout, motion, useMotionValue} from "framer-motion";
 import Accordion from "./Accordion";
 import './style.css';
 import Switch from "./Switch";
@@ -7,7 +7,12 @@ export default function App () {
     const accordionIds = [0, 1, 2, 3];
     const [expanded, setExpanded] = useState<false | number>(0);
     const [isActive, setIsActive] = useState<false | number>(0);
+    const [isVisible, setVisible] = useState(true);
     const [index, setIndex] = useState(false);
+
+    const ref = useRef(null);
+    const { top, bottom } = useScrollConstraints(ref);
+    const y = useMotionValue(0);
 
     return (
         <div>
@@ -24,26 +29,58 @@ export default function App () {
                     repeatType: "reverse"
                 }}/>
                 <motion.div className="motion-div"
-                    whileHover={{scale: 1.1, rotate: 90}}
+                            whileHover={{scale: 1.1, rotate: 90}}
                 />
                 <motion.div className="motion-div"
-                    whileTap={{scale: 0.9, rotate: 90, borderRadius: "100%"}}
-                    transition={{duration: 0.6}}
+                            whileTap={{scale: 0.9, rotate: 90, borderRadius: "100%"}}
+                            transition={{duration: 0.6}}
                 />
                 <motion.div className="motion-div"
-                    initial={{opacity: 0.3}}
-                    whileHover={{opacity: 1}}
-                    transition={{duration: 0.6}}
+                            initial={{opacity: 0.3}}
+                            whileHover={{opacity: 1}}
+                            transition={{duration: 0.6}}
                 />
+                <motion.div
+                    className="motion-div"
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                    onTap={() => setVisible(!isVisible)}
+                >
+                    <AnimatePresence>
+                        {isVisible && (
+                            <motion.div
+                                style={{
+                                    width: 80,
+                                    height: 80,
+                                    borderRadius: 15,
+                                    backgroundColor: "#fff"
+                                }}
+                                initial={{opacity: 0, scale: 0.75}}
+                                animate={{opacity: 1, scale: 1}}
+                                exit={{opacity: 0, scale: 0}}
+                            />
+                        )}
+                    </AnimatePresence>
+                </motion.div>
                 <motion.input whileFocus={{backgroundColor: "yellow"}}/>
-                <Switch isActive={isActive} setIsActive={setIsActive} />
-                <Gallery items={colors} setIndex={setIndex} />
+                <Switch isActive={isActive} setIsActive={setIsActive}/>
+            </div>
+            <div className="container">
+                <div className="container-accordion">
+                    {accordionIds.map((accordionId) => (
+                        <Accordion id={accordionId} expanded={expanded} setExpanded={setExpanded}/>
+                    ))}
+                </div>
+                <Gallery items={colors} setIndex={setIndex}/>
                 <AnimatePresence>
                     {index !== false && (
                         <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 0.6 }}
-                            exit={{ opacity: 0 }}
+                            initial={{opacity: 0}}
+                            animate={{opacity: 0.6}}
+                            exit={{opacity: 0}}
                             key="overlay"
                             className="overlay"
                             onClick={() => setIndex(false)}
@@ -60,31 +97,42 @@ export default function App () {
                         />
                     )}
                 </AnimatePresence>
-            </div>
-            <div className="container">
-                <div className="container-accordion">
-                    {accordionIds.map((accordionId) => (
-                        <Accordion id={accordionId} expanded={expanded} setExpanded={setExpanded} />
-                ))}
+                <div className="scroll-container" ref={ref}>
+                    <motion.div
+                        drag="y"
+                        dragConstraints={{top, bottom}}
+                        className="scrollable"
+                        style={{y}}
+                    >
+                        <div className="item"/>
+                        <div className="item"/>
+                        <div className="item"/>
+                        <div className="item"/>
+                        <div className="item"/>
+                        <div className="item"/>
+                        <div className="item"/>
+                        <div className="item"/>
+                    </motion.div>
                 </div>
             </div>
+
         </div>
     );
 }
 
-function SingleImage({ color, onClick }) {
+function SingleImage({color, onClick}) {
     return (
         <div className="single-image-container" onClick={onClick}>
             <motion.div
                 layoutId={color}
                 className="single-image"
-                style={{ backgroundColor: color }}
+                style={{backgroundColor: color}}
             />
         </div>
     );
 }
 
-function Gallery({ items, setIndex }) {
+function Gallery({items, setIndex}) {
     return (
         <ul className="gallery-container">
             {items.map((color, i) => (
@@ -92,7 +140,7 @@ function Gallery({ items, setIndex }) {
                     className="gallery-item"
                     key={color}
                     onClick={() => setIndex(i)}
-                    style={{ backgroundColor: color }}
+                    style={{backgroundColor: color}}
                     layoutId={color}
                 />
             ))}
@@ -105,3 +153,17 @@ const makeColor = hue => `hsl(${hue}, 100%, 50%)`;
 const colors = Array.from(Array(numColors)).map((_, i) =>
     makeColor(Math.round((360 / numColors) * i))
 );
+
+function useScrollConstraints(ref) {
+    const [constraints, setConstraints] = useState({ top: 0, bottom: 0 });
+
+    useEffect(() => {
+        const element = ref.current;
+        const viewportHeight = element.offsetHeight;
+        const contentHeight = element.firstChild.offsetHeight;
+
+        setConstraints({ top: viewportHeight - contentHeight, bottom: 0 });
+    }, []);
+
+    return constraints;
+}
